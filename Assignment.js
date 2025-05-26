dc8c28a846db2584c16f055af8b54dc804463a5b
Kora <you@example.com>
2025-05-26 13:26:52 +0100

    first commit

--- Assignment.js
+++ Assignment.js
@@ -1,17 +1,17 @@
-let StudentName = 'Favour';
-console.log(StudentName)
+let StudentName = "Favour";
+console.log(StudentName);
 
 let TestScore1 = 157;
-console.log(TestScore1)
+console.log(TestScore1);
 
 let TestScore2 = 208;
-console.log(TestScore2)
+console.log(TestScore2);
 
-let Sum = (TestScore1 + TestScore2)
-console.log(Sum)
+let Sum = TestScore1 + TestScore2;
+console.log(Sum);
 
-let AverageScore = (Sum / 2);
-console.log(AverageScore)
+let AverageScore = Sum / 2;
+console.log(AverageScore);
 
 const PassMark = 200;
-console.log(PassMark)
+console.log(PassMark);
