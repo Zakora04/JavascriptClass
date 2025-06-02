d75a5defebb0bad1afb520467da89fb9c9979c3d
zakora <chichetawin@gmail.com>
2025-06-02 22:03:07 +0100

    conditions.js

---
+++ conditions.js
@@ -0,0 +1,30 @@
+//The If statement
+let age = 16;
+
+if (age < 18) {
+  console.log("not for persons under the age of 18");
+}
+
+//if.....else
+
+let temperature = 37;
+if (temperature === 37) {
+  console.log("temperature is normal");
+} else {
+  console.log("temperature low");
+}
+
+let jambscore = "";
+let jambage = "";
+
+if (jambscore >= 200 && jambage >= 18) {
+  console.log("Admission Granted");
+} else {
+  console.log("try again next year");
+}
+let time = "";
+if (time < 12) {
+  console.log("good morning");
+} else if (time >= 13) {
+  console.log("Good afternoon");
+}
