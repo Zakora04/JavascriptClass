bb0e5ace1da97228121f9a4fe8476ff8bcedd53f
Kora <you@example.com>
2025-06-02 21:43:44 +0100

    new file

--- object.js
+++ object.js
@@ -36,8 +36,10 @@ const usersettings = { fontsize: 18 };
 
 const finalSetting = Object.assign({}, defaultsettings, usersettings);
 console.log(finalSetting);
+delete usersettings.defaultsettings;
+console.log(defaultsettings);
 
-//object.Entries
+//object.FromEntries
 
 const ent = [
   ["name", "tosin"],
@@ -49,3 +51,56 @@ console.log(Object.fromEntries(ent));
 const original = { A: 77, B: 66 };
 const final = { ...original, C: 55 };
 console.log(final);
+
+//delete property
+const user = {
+  name: "alice",
+  age: 40,
+  email: "gfwfbweh@gmail.com",
+};
+
+console.log(user);
+delete user.email;
+console.log(user);
+
+//Object.keys
+const keys = Object.keys(user);
+console.log(keys);
+
+//Object.values
+const values = Object.values(user);
+console.log(values);
+
+//Object.entries
+const entries = Object.entries(user);
+console.log(entries);
+const swapped = Object.fromEntries(
+  Object.entries(user).map(([key, value]) => [value, key])
+);
+console.log(swapped);
+//Object.freeze
+
+const names = {
+  firstname: "ada",
+  secondname: "adaobi",
+  ages: {
+    duse: 79,
+  },
+};
+console.log(names);
+const freeze = Object.freeze(user);
+console.log(freeze);
+
+Object.seal(names);
+
+names.firstname = "kamfe";
+names.ages.duse = 87;
+console.log(names);
+const profile = {
+  name: "rerrt",
+  age: "fthrv",
+};
+//profile.name = "tolani";
+//console.log(profile);
+delete profile.name;
+console.log(profile);
